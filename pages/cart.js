e90e2a2a0215eed1d7e877d23f1c7ba073723141
Paul <isakura313@gmail.com>
2022-01-18 00:21:36 +0300

    i fix the cart

--- pages/cart.js
+++ pages/cart.js
@@ -7,17 +7,24 @@ import { Row, Col } from 'antd';
 function Cart(){
     const[cartProducts, setCartProducts] = useState([])
     useEffect(() => {
+      const getData = async() => {
+        const productList = []
         const ids = JSON.parse(localStorage.getItem("products"))
-        const getData = [];
-        ids.map((async(id) =>{
-           const product =  await axios({
+        if(!ids){
+          return 
+        }
+        ids.map(async(id)=>{
+           return await axios({
                 method: "GET",
                 url: `http://localhost:5000/products/${id}`,
-              });
-              getData.push(product.data)
-        }))
-        setCartProducts(getData)
-        console.log(getData)
+              }).then((res)=>{
+                productList.push(res.data);
+                setCartProducts(prevArray => [...prevArray, res.data])
+              })
+            })
+            console.log(productList[0])
+          }
+          getData()
       }, []);
 
       const productList = cartProducts.map((product) => {
@@ -33,12 +40,17 @@ function Cart(){
           </Col>
         );
       });
-return(
+      if(cartProducts.length=== 0){
+        return <h1> Нет товаров! Добавьте сюда что-нибудь!</h1>
+      } else{
+      return(
     <div>
+      
         {productList}
         <h1 className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"> Это страница Cart</h1>
     </div>
 )
+}
 }
 
 export default Cart
